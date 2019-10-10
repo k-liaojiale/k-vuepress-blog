ac5e977ebb2de943b785df520f6731351a54fec9
liaojiale <k.liaojiale@gmail.com>
2019-10-10 18:00:52 +0800

    变更github推送地址

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:Luke199711/Luke199711.github.io.git master
+git push -f git@github.com:k-liaojiale/k-liaojiale.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
